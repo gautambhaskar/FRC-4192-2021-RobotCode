36c26c23894016ff04e7188b122148550889356b
Gautam Bhaskar <gautambhaskar@outlook.com>
2020-10-09 16:55:50 -0500

    added end method for intake commands to set motor speed to 0 upon end

--- src/main/java/frc/robot/commands/IntakeBalls.java
+++ src/main/java/frc/robot/commands/IntakeBalls.java
@@ -38,6 +38,7 @@ public class IntakeBalls extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    m_intake.intake(0);
   }
 
   // Returns true when the command should end.
