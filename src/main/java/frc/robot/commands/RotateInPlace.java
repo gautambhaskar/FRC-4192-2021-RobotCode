3897bff93e6c57ea936115d104318e5cc61cd970
KingChad127 <48454518+KingChad127@users.noreply.github.com>
2020-11-30 23:15:09 -0600

    set rotation to 0 after command runs

--- src/main/java/frc/robot/commands/RotateInPlace.java
+++ src/main/java/frc/robot/commands/RotateInPlace.java
@@ -39,6 +39,7 @@ public class RotateInPlace extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    m_drive.arcadeDrive(0, 0);
   }
 
   // Returns true when the command should end.
