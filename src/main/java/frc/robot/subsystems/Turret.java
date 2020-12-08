f5a8fe2ce90b4990e9b1aba1e4621bfe8d14020d
Gautam Bhaskar <gautambhaskar@outlook.com>
2020-12-08 17:09:19 -0600

    cargo cam

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -62,7 +62,7 @@ public class Turret extends SubsystemBase {
 
   private HttpCamera limelightFeed;
   private MjpegServer server;
-  private UsbCamera cargoCam;
+  private MjpegServer cargoCam;
 
   NetworkTableEntry limelightX, limelightY, limelightA, visionMode, turretSetOutput, turretAppliedOutput, turretAngle;
   NetworkTableEntry turretPosition, turretVelocity, turretDirection;
@@ -77,13 +77,13 @@ public class Turret extends SubsystemBase {
     limelightA = cameraTab.add("LimelightArea", ta.getDouble(0)).getEntry();
 
     limelightFeed = new HttpCamera("limelight", "http://limelight.local:5800/stream.mjpg");
-    cargoCam = CameraServer.getInstance().startAutomaticCapture(0);
-    cargoCam.setConnectVerbose(0);
+    cargoCam = CameraServer.getInstance().addServer("fisheye");
     Shuffleboard.selectTab("Camera");
 
     cameraTab.add("Camera Feed", limelightFeed).withWidget(BuiltInWidgets.kCameraStream).withPosition(0, 0).withSize(3,
         3);
-    cameraTab.add("Fisheye Feed", cargoCam).withWidget(BuiltInWidgets.kCameraStream).withPosition(4, 0).withSize(3, 3);
+    cameraTab.add("Fisheye Feed", cargoCam.getSource()).withWidget(BuiltInWidgets.kCameraStream).withPosition(4, 0)
+        .withSize(3, 3);
 
     visionMode = cameraTab.add("Camera Mode", "Camera").getEntry();
 
