a45ac6725da04d190108afd3aabbbf41f757b772
Rohit Shetty <shettyr@go.lisd.net>
2020-12-15 13:15:21 -0600

    rotate drive command

---
+++ src/main/java/frc/robot/commands/RotateDrive.java
@@ -0,0 +1,57 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+
+public class RotateDrive extends CommandBase {
+  /**
+   * Creates a new RotateDrive.
+   */
+   private final Drivetrain m_drive;
+   private final double m_rotate;
+
+  public RotateDrive(Drivetrain d, double r) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    m_drive = d;
+    m_rotate = r;
+    addRequirements(x_drive);
+
+  }
+
+  /*
+  public void arcadeDrive(double fwd, double turn) {
+    m_drive.arcadeDrive(fwd, turn); // <<<<<<Need to invert motors instead >>>>>>>
+  }
+  */
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_drive.arcadeDrive(0,r);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_drive.arcadeDrive(0,0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
