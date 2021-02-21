9d8926fb58f10491d086c83655740e0c8380a30a
Gautam Bhaskar <gautambhaskar@outlook.com>
2021-02-21 15:52:45 -0600

    imported drivePID

--- src/main/java/frc/robot/commands/autonomous/RedSearchAutonA.java
+++ src/main/java/frc/robot/commands/autonomous/RedSearchAutonA.java
@@ -7,9 +7,11 @@ package frc.robot.commands.autonomous;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.Constants;
+import frc.robot.Constants.drivePID;
 import frc.robot.commands.drive.DriveForDistance;
 import frc.robot.commands.drive.RotateInPlace;
 import frc.robot.subsystems.Intake;
+
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
@@ -18,10 +20,10 @@ public class RedSearchAutonA extends SequentialCommandGroup {
   public RedSearchAutonA(Drivetrain m_drive, Intake m_intake) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new DriveAndIntake(m_drive, m_intake, 5*drivePID.feetToRotations), new RotateInPlace(m_drive, 68.20), 
-        new DriveAndIntake(m_drive, m_intake, 5.6*drivePID.feetToRotations), new RotateInPlace(m_drive, -140), 
-        new DriveAndIntake(m_drive, m_intake, 7.9*drivePID.feetToRotations), new RotateInPlace(m_drive, 71.57),
-        new DriveAndIntake(m_drive, m_intake, 12.5*drivePID.feetToRotations));
-    //Replace 0 with correct distances    
+    addCommands(new DriveAndIntake(m_drive, m_intake, 5 * drivePID.feetToRotations), new RotateInPlace(m_drive, 68.20),
+        new DriveAndIntake(m_drive, m_intake, 5.6 * drivePID.feetToRotations), new RotateInPlace(m_drive, -140),
+        new DriveAndIntake(m_drive, m_intake, 7.9 * drivePID.feetToRotations), new RotateInPlace(m_drive, 71.57),
+        new DriveAndIntake(m_drive, m_intake, 12.5 * drivePID.feetToRotations));
+    // Replace 0 with correct distances
   }
 }
