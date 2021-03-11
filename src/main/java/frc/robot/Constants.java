5c5b6e85583b36ac5e45a8451078e5d11eecc2c8
Gautam Bhaskar <gautambhaskar@outlook.com>
2021-03-11 17:26:09 -0600

    a

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -50,7 +50,7 @@ public final class Constants {
     public static double outtakeSlowlySpeed = 0.3;
     public static double indexSpeed = 0.8;
     public static double feederSpeed = -3000;
-    public static double shooterSpeed = 4000;
+    public static double shooterSpeed = 4500;
     public static double hoodRotateSpeed = 0.3;
     public static double turretTurn = 0.7;
     public static double indexRunTime = 0.25;
@@ -74,13 +74,13 @@ public final class Constants {
     }
 
     public static class shooterPID {
-        public static double kP = 0.001;
+        public static double kP = 0.0015;
         public static double kI = 0.000001;
         public static double kD = 0;
         public static double kFF = 0.0002;
         public static double kMax = 0.9;
         public static double kMin = -0.9;
-        public static double shooterSpeedMinimum = 3700;
+        public static double shooterSpeedMinimum = 4300;
     }
 
     public static class feederPID {
