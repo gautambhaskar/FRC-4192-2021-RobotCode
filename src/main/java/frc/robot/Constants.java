cac1fe6ce07b8b735eda56303d5cbe0efe2070d8
Gautam Bhaskar <gautambhaskar@outlook.com>
2021-03-11 17:33:27 -0600

    a

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -74,7 +74,7 @@ public final class Constants {
     }
 
     public static class shooterPID {
-        public static double kP = 0.002;
+        public static double kP = 0.0023;
         public static double kI = 0.000001;
         public static double kD = 0;
         public static double kFF = 0.0002;
