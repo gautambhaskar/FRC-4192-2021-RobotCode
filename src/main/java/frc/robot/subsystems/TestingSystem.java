31baac067a599c1e826851630ea6f6b0c6e2e5dc
Gautam Bhaskar <gautambhaskar@outlook.com>
2021-03-15 11:19:23 -0500

    added test motor indicator to subsystems tab

--- src/main/java/frc/robot/subsystems/TestingSystem.java
+++ src/main/java/frc/robot/subsystems/TestingSystem.java
@@ -7,6 +7,9 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -16,17 +19,25 @@ public class TestingSystem extends SubsystemBase {
    * Creates a new TestingSystem.
    */
   private final CANSparkMax m_motor = new CANSparkMax(4, MotorType.kBrushless);
+  private final ShuffleboardTab subsystemsTab = Shuffleboard.getTab("Subsystems");
+  private NetworkTableEntry testMotorEngaged = subsystemsTab.add("Test Motor engaged", false).getEntry();
 
   public TestingSystem() {
   }
 
   @Override
   public void periodic() {
+
     // This method will be called once per scheduler run
   }
 
   // runs motor with power spd
   public void runMotor(double spd) {
     m_motor.set(spd);
+    if (spd != 0) {
+      testMotorEngaged.setBoolean(true);
+    } else {
+      testMotorEngaged.setBoolean(false);
+    }
   }
 }
