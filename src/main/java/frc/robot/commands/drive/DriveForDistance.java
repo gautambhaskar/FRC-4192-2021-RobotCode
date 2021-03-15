40a53c9dca13495b0d22f158431b1c684586aa35
Gautam Bhaskar <gautambhaskar@outlook.com>
2021-03-15 11:50:46 -0500

    negate distance on drivetrain

--- src/main/java/frc/robot/commands/drive/DriveForDistance.java
+++ src/main/java/frc/robot/commands/drive/DriveForDistance.java
@@ -26,7 +26,7 @@ public class DriveForDistance extends PIDCommand {
         // This should return the measurement
         () -> m_drive.returnDrivetrainPosition(),
         // This should return the setpoint (can also be a constant)
-        () -> m_distance,
+        () -> -m_distance,
         // This uses the output
         output -> {
           if (output > drivePID.autonMaxSpeed) {
